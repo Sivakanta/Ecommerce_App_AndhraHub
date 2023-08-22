210880a69e8598d9896954aa2a61b1c1e9a1884e
Sivakanta <125561206+Sivakanta@users.noreply.github.com>
2023-08-22 19:04:16 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,18 @@ import Register from './pages/Auth/Register';
 import Login from './pages/Auth/Login';
 import { Toaster } from 'react-hot-toast';
 import 'react-toastify/dist/ReactToastify.css';
+import Dashboard from './pages/User/Dashboard';
+import PrivateRoute from "./components/Routes/Private";
+
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home /> }  />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="" element={<Dashboard />} />
+        </Route>  
         <Route path='/Register' element={<Register /> }  />
         <Route path='/Login' element={<Login /> }  />
         <Route path='/About' element={<About /> }  />
